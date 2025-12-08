1034ec5931a8167246635c7e8cc3d9779a63a392
Youssef Elsedik Moustafa <youssefelsedikmoustafa@gmail.com>
2025-12-08 14:21:26 +0100

    fix pl imoprt

--- frontend/src/components/InternshipsAssignment/InternshipAssignmentStudentForm.tsx
+++ frontend/src/components/InternshipsAssignment/InternshipAssignmentStudentForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from "react";
 import StudentConfigService from "../../services/studentConfigService";
 import "../../styles/InternshipsAssignment/InternshipAssignmentStudentForm.css";
